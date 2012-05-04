fe332b9f72a6ff3ce9d0b76599d5734c4898989d
Wayne Graham <wayne.graham@virginia.edu>
2012-05-04 10:31:16 -0400

    Refactor plugin in to static TeiPlugin class; file formatting cleanup

--- models/TeiDisplay/File.php
+++ models/TeiDisplay/File.php
@@ -2,41 +2,36 @@
 /**
  * TeiDisplay_File class
  *
- * @copyright  Scholars' Lab 2010
- * @license    
- * @version    $Id:$
- * @author Ethan Gruber
- * 
  * Used to get listing of xslt files in libraries folder
- **/
+ */
 class TeiDisplay_File 
 {
-	protected $_fileName;
+  protected $_fileName;
 
-	public static function getFiles() 
-	{
-	    $fileNames = array();
-        $paths = new DirectoryIterator(TEI_DISPLAY_STYLESHEET_FOLDER);
-        foreach ($paths as $file) {
-            if (!$file->isDot() && !$file->isDir()) {
-                if (strrchr($file, '.') == '.xsl') {
-                    $fileNames[] = $file->getFilename();                    
-                }
-            }
+  public static function getFiles()
+  {
+    $fileNames = array();
+    $paths = new DirectoryIterator(TEI_DISPLAY_STYLESHEET_FOLDER);
+    foreach ($paths as $file) {
+      if (!$file->isDot() && !$file->isDir()) {
+        if (strrchr($file, '.') == '.xsl') {
+          $fileNames[] = $file->getFilename();
         }
-        
-        // sort the files by filenames
-        natsort($fileNames); 
-        return $fileNames;
-	}	
-	
-	/**
-    * Get the file name for the file
-    * 
-    * @return string
-    */	
- 	public function getFileName() 
- 	{
- 	    return $this->_fileName;
- 	}
+      }
+    }
+
+    // sort the files by filenames
+    natsort($fileNames);
+    return $fileNames;
+  }	
+
+  /**
+   * Get the file name for the file
+   * 
+   * @return string
+   */	
+  public function getFileName()
+  {
+    return $this->_fileName;
+  }
 }
