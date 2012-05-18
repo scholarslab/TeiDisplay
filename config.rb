9299416aba750f1a55287b5c7fbad34142304625
Wayne Graham <wayne.graham@virginia.edu>
2012-05-17 20:52:56 -0400

    pushes the css in to SCSS

---
+++ config.rb
@@ -0,0 +1,11 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "views/shared/css"
+sass_dir = "_sass"
+images_dir = "views/public/images"
+javascripts_dir = "views/public/javascripts"
+output_style = :compressed # expanded, nested, compact, or compressed
+relative_assets = false
+line_comments = false
