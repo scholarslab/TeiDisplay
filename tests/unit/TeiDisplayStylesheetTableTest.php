f83de5639de160e8c0c69fdb1e36540512bbd968
David McClure <david.mcclure@virginia.edu>
2012-09-25 12:01:09 -0400

    Adds test scaffolding.

---
+++ tests/unit/TeiDisplayStylesheetTableTest.php
@@ -0,0 +1,27 @@
+<?php
+
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4; */
+
+/**
+ * Stylesheet table tests.
+ *
+ * @package     omeka
+ * @subpackage  teidisplay
+ * @copyright   2012 Rector and Board of Visitors, University of Virginia
+ * @license     http://www.apache.org/licenses/LICENSE-2.0.html
+ */
+
+class TeiDisplay_TeiDisplayStylesheetTableTest extends TeiDisplay_Test_AppTestCase
+{
+
+    /**
+     * Test.
+     *
+     * @return void.
+     */
+    public function testStub()
+    {
+
+    }
+
+}
---
+++ tests/unit/TeiDisplayStylesheetTest.php
@@ -0,0 +1,27 @@
+<?php
+
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4; */
+
+/**
+ * Stylesheet row tests.
+ *
+ * @package     omeka
+ * @subpackage  teidisplay
+ * @copyright   2012 Rector and Board of Visitors, University of Virginia
+ * @license     http://www.apache.org/licenses/LICENSE-2.0.html
+ */
+
+class TeiDisplay_TeiDisplayStylesheetTest extends TeiDisplay_Test_AppTestCase
+{
+
+    /**
+     * Test.
+     *
+     * @return void.
+     */
+    public function testStub()
+    {
+
+    }
+
+}
