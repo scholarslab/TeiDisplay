ec226cfbeda97bbb8303ef6fc0e2e07c33e5cb64
David McClure <david.mcclure@virginia.edu>
2012-09-26 10:17:39 -0400

    Shell for stylesheets controller.

---
+++ controllers/StylesheetsController.php
@@ -0,0 +1,78 @@
+<?php
+
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4; */
+
+/**
+ * XSLT stylesheets controller.
+ *
+ * @package     omeka
+ * @subpackage  teidisplay
+ * @copyright   2012 Rector and Board of Visitors, University of Virginia
+ * @license     http://www.apache.org/licenses/LICENSE-2.0.html
+ */
+
+class TeiDisplay_StylesheetsController extends Omeka_Controller_AbstractActionController
+{
+
+    /**
+     * Create stylesheet.
+     *
+     * @return void
+     */
+    public function addAction()
+    {
+
+    }
+
+    /**
+     * Edit stylesheet.
+     *
+     * @return void
+     */
+    public function editAction()
+    {
+
+    }
+
+    /**
+     * Set add success message.
+     */
+    protected function _getAddSuccessMessage($stylesheet)
+    {
+        return __('The stylesheet "%s" was successfully added!', $stylesheet->title);
+    }
+
+    /**
+     * Set edit success message.
+     */
+    protected function _getEditSuccessMessage($neatline)
+    {
+        return __('The Neatline "%s" was successfully changed!', $stylesheet->title);
+    }
+
+    /**
+     * Set delete success message.
+     */
+    protected function _getDeleteSuccessMessage($neatline)
+    {
+        return __('The Neatline "%s" was successfully deleted!', $stylesheet->title);
+    }
+
+    /**
+     * Set delete confirm message.
+     */
+    protected function _getDeleteConfirmMessage($neatline)
+    {
+        return __('This will delete the Neatline "%s" and its associated metadata.', $neatline->name);
+    }
+
+    /**
+     * Construct the add/edit form.
+     */
+    private function _getForm(TeiDisplayStylesheet $stylesheet)
+    {
+        $form = new TeiDisplay_Form_Stylesheet(array('stylesheet' => $stylesheet));
+        return $form;
+    }
+
+}
