c5923bf49e22236e342fd2ed763a3fed5a54b15c
David McClure <david.mcclure@virginia.edu>
2012-10-03 11:00:56 -0400

    Shell for TeiDisplayText model.

---
+++ models/TeiDisplayText.php
@@ -0,0 +1,44 @@
+<?php
+
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4; */
+
+/**
+ * Row class for XML text.
+ *
+ * @package     omeka
+ * @subpackage  teidisplay
+ * @copyright   2012 Rector and Board of Visitors, University of Virginia
+ * @license     http://www.apache.org/licenses/LICENSE-2.0.html
+ */
+
+
+class TeiDisplayText extends Omeka_Record_AbstractRecord
+{
+
+
+    /**
+     * The XSLT stylesheet.
+     * tinytext collate utf8_unicode_ci
+     */
+    // public $title;
+
+    /**
+     * The XSLT stylesheet.
+     * TEXT COLLATE utf8_unicode_ci NULL
+     */
+    // public $xslt;
+
+    /**
+     * The date the stylesheet was last modified.
+     * TIMESTAMP NULL
+     */
+    // public $modified;
+
+
+    /**
+     * Zend_Date format for MySQL timestamp.
+     */
+    // const DATE_FORMAT = 'yyyy-MM-dd HH:mm:ss';
+
+
+}
